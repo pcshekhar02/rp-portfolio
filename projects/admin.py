823e9589b204cd6b9d78680bc01afdee65fe665a
pcshekhar02 <pcshekhar02@gmail.com>
2024-07-04 18:49:11 +0530

    initial commit

---
+++ projects/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+# projects/admin.py
+
+from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
